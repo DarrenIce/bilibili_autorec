6224559b0343887e6dd02ff5f1af03c123d490a5
DarrenIce <bhl865931168@gmail.com>
2020-12-08 18:14:54 +0800

    增加上传至百度云脚本

---
+++ backup2baidu.py
@@ -0,0 +1,24 @@
+import subprocess
+import os
+import datetime
+from utils.live import Live
+
+PCSpath = r'.\utils\BaiduPCS-Go.exe'
+local_base_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'rec')
+pcs_base_path = '录播/%s'
+live = Live()
+
+for key in live.live_infos:
+    name = live.live_infos[key]['uname']
+    a = subprocess.run([PCSpath,"mkdir",pcs_base_path % name])
+    print(a)
+    base_path = os.path.join(local_base_path,name)
+    if os.path.exists(base_path):
+        for f in os.listdir(base_path):
+            if os.path.isfile(os.path.join(base_path,f)):
+                if '_mask' not in f:
+                    x = f.rstrip('.mp4').lstrip(name+'_')
+                    if (datetime.date.today() - datetime.timedelta(days=1)).strftime('%Y%m%d') == x:
+                        a = subprocess.run([PCSpath,'upload',os.path.join(base_path,f),pcs_base_path % name])
+                        print(a)
+
