b091898decc1ca210c3c537d07e211cd42dc29af
DarrenIce <bhl865931168@gmail.com>
2020-12-09 17:11:27 +0800

    增加了每天自动上传到百度云的定时任务

--- backup2baidu.py
+++ backup2baidu.py
@@ -4,16 +4,23 @@ import datetime
 from utils.live import Live
 from apscheduler.schedulers.blocking import BlockingScheduler
 import keyboard
+import threading
+from utils.log import Log
+import time
+
+logger = Log()()
 
 
-PCSpath = r'.\utils\BaiduPCS-Go.exe'
-local_base_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'rec')
-pcs_base_path = '录播/%s'
+
 live = Live()
 keyboard.add_hotkey(r'ctrl+/', os._exit, args=[0])
-def job():
-    for key in live.live_infos:
-        name = live.live_infos[key]['uname']
+def daily_job():
+    PCSpath = r'.\utils\BaiduPCS-Go.exe'
+    local_base_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'rec')
+    pcs_base_path = '录播/%s'
+    live_infos = live.live_infos.copy()
+    for key in live_infos:
+        name = live_infos[key]['uname']
         base_path = os.path.join(local_base_path,name)
         if os.path.exists(base_path):
             a = subprocess.run([PCSpath,"mkdir",pcs_base_path % name])
@@ -26,8 +33,15 @@ def job():
                             a = subprocess.run([PCSpath,'upload',os.path.join(base_path,f),pcs_base_path % name])
                             print(a)
 
-# scheduler = BlockingScheduler()
-# scheduler.add_job(job,'cron',day_of_week='0-6',hour=7,minute=00)
-# scheduler.start()
-job()
+def test():
+    logger.info(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
+def scheduler_run():
+    scheduler = BlockingScheduler()
+    scheduler.add_job(test,'cron',day_of_week='0-6',hour=17,minute=10)
+    scheduler.start()
+
+threading.Thread(target=scheduler_run,daemon=True).start()
+while True:
+    time.sleep(1)
 # a = a = subprocess.run([PCSpath,"share","set",'录播'])
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from utils.live import Live
 import os
+import subprocess
+import threading
+from apscheduler.schedulers.blocking import BlockingScheduler
+import datetime 
 import keyboard
 
+live = Live()
 
 def main():
     log_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'log')
@@ -20,9 +25,32 @@ def main():
     if not os.path.exists(ffmpeg_log_file):
         with open(ffmpeg_log_file, 'w', encoding='utf-8') as a:
             pass
-    live = Live()
+    threading.Thread(target=scheduler_run,daemon=True).start()
     live.run()
 
+def daily_job():
+    PCSpath = r'.\utils\BaiduPCS-Go.exe'
+    local_base_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'rec')
+    pcs_base_path = '录播/%s'
+    live_infos = live.live_infos.copy()
+    for key in live_infos:
+        name = live_infos[key]['uname']
+        base_path = os.path.join(local_base_path,name)
+        if os.path.exists(base_path):
+            a = subprocess.run([PCSpath,"mkdir",pcs_base_path % name])
+            print(a)
+            for f in os.listdir(base_path):
+                if os.path.isfile(os.path.join(base_path,f)):
+                    if '_mask' not in f:
+                        x = f.rstrip('.mp4').lstrip(name+'_')
+                        if (datetime.date.today() - datetime.timedelta(days=1)).strftime('%Y%m%d') == x:
+                            a = subprocess.run([PCSpath,'upload',os.path.join(base_path,f),pcs_base_path % name])
+                            print(a)
+
+def scheduler_run():
+    scheduler = BlockingScheduler()
+    scheduler.add_job(daily_job,'cron',day_of_week='0-6',hour=7,minute=1)
+    scheduler.start()
 
 if __name__ == '__main__':
     main()
