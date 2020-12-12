17b755bb4c5dd97cf4e3d81c18a80d9c41f12604
DarrenIce <bhl865931168@gmail.com>
2020-12-12 11:29:57 +0800

    up

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import threading
 from apscheduler.schedulers.blocking import BlockingScheduler
 import datetime 
 import keyboard
-from utils.threadRecoder import threadRecorder
 
 log_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'log')
 keyboard.add_hotkey('ctrl+alt+;', os._exit, args=[0])
@@ -24,6 +23,7 @@ if not os.path.exists(ffmpeg_log_file):
         pass
 
 from utils.live import Live
+from utils.threadRecoder import threadRecorder
 live = Live()
 threadRecorder = threadRecorder()
 
