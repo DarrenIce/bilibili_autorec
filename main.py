90a90d2af7e022ee2a836eb17baa41142f8763cd
DarrenIce <bhl865931168@gmail.com>
2020-12-12 11:28:12 +0800

    up

--- main.py
+++ main.py
@@ -7,26 +7,27 @@ import datetime
 import keyboard
 from utils.threadRecoder import threadRecorder
 
+log_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'log')
+keyboard.add_hotkey('ctrl+alt+;', os._exit, args=[0])
+if not os.path.exists(log_path):
+    os.mkdir(log_path)
+log_file = os.path.join(log_path, 'log.log')
+stream_log_file = os.path.join(log_path, 'stream.log')
+ffmpeg_log_file = os.path.join(log_path, 'ffmpeg.log')
+if not os.path.exists(log_file):
+    with open(log_file, 'w', encoding='utf-8') as a:
+        pass
+if not os.path.exists(stream_log_file):
+    with open(stream_log_file, 'w', encoding='utf-8') as a:
+        pass
+if not os.path.exists(ffmpeg_log_file):
+    with open(ffmpeg_log_file, 'w', encoding='utf-8') as a:
+        pass
+
 live = Live()
 threadRecorder = threadRecorder()
 
 def main():
-    log_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'log')
-    keyboard.add_hotkey('ctrl+alt+;', os._exit, args=[0])
-    if not os.path.exists(log_path):
-        os.mkdir(log_path)
-    log_file = os.path.join(log_path, 'log.log')
-    stream_log_file = os.path.join(log_path, 'stream.log')
-    ffmpeg_log_file = os.path.join(log_path, 'ffmpeg.log')
-    if not os.path.exists(log_file):
-        with open(log_file, 'w', encoding='utf-8') as a:
-            pass
-    if not os.path.exists(stream_log_file):
-        with open(stream_log_file, 'w', encoding='utf-8') as a:
-            pass
-    if not os.path.exists(ffmpeg_log_file):
-        with open(ffmpeg_log_file, 'w', encoding='utf-8') as a:
-            pass
     threading.Thread(target=threadRecorder.heartbeat).start()
     threadRecorder.add('daily_job',scheduler_run,None,False)
     live.start()
