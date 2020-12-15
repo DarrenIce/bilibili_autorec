76b055c0eb1d0584fb954f0f623b45afaf2b8b07
DarrenIce <bhl865931168@gmail.com>
2020-12-15 20:17:57 +0800

    up

--- backup2baidu.py
+++ backup2baidu.py
@@ -17,13 +17,13 @@ keyboard.add_hotkey(r'ctrl+/', os._exit, args=[0])
 def daily_job():
     PCSpath = r'.\utils\BaiduPCS-Go.exe'
     local_base_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'rec')
-    pcs_base_path = '录播/%s'
+    pcs_base_path = '/录播/%s'
     live_infos = live.live_infos.copy()
     for key in live_infos:
         name = live_infos[key]['uname']
         base_path = os.path.join(local_base_path,name)
         if os.path.exists(base_path):
-            a = subprocess.run([PCSpath,"mkdir",pcs_base_path % name])
+            a = subprocess.run([PCSpath,"cd",pcs_base_path % name])
             print(a)
             for f in os.listdir(base_path):
                 if os.path.isfile(os.path.join(base_path,f)):
